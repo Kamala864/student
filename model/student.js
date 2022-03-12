3a7f46ed3419423dc0851f03d3d0b0eeecee2a36
Kamala864 <56905936+Kamala864@users.noreply.github.com>
2022-03-12 07:37:56 +0545

    initial commit

---
+++ model/student.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const Student = new mongoose.Schema(
+    {
+        courseTitle:{
+            type: String,
+        },
+        courseContent:{
+            type: String,
+        },
+        courseType:{
+            type: String,
+        },
+        coursePrice:{
+            type: String,
+
+        },
+        courseAuthor:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        coursePic: {
+            type: String,
+            default: "no-photo.jpg",
+          },
+          createdAt: {
+            type: Date,
+            default: Date.now,
+          },
+    }
+);
+
+module.exports = mongoose.model("Student",Student);
